9dd0827ae18bf84e474fe2b6c29a3f0ca7a76a69
ryanlazo <ryanlazo12@gmail.com>
2018-04-03 11:47:14 -0600

    Initial commit

---
+++ src/edu/cnm/deepdive/rps/Main.java
@@ -0,0 +1,14 @@
+package edu.cnm.deepdive.rps;
+
+import edu.cnm.deepdive.rps.controller.Player;
+
+
+public class Main {
+
+  public static void main (String[] args) {
+    Player player = new Player();
+    player.init();
+    player.start();
+  }
+
+}
